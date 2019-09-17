9adac53eefd1ba76c10a566e138b5bc79c0023d8
Robin Kipsch <robin.kipsch@rio.cloud>
2019-09-17 17:13:36 +0200

    (refactor) allow deep-linking for iFrame pages; remove unused polyfills and dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const iFrameInitialSize = 550;
+const IFRAME_INITIAL_SIZE = 550;
 
 class IframeResizer extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            height: iFrameInitialSize,
+            height: IFRAME_INITIAL_SIZE,
         };
         this.receiveMessage = this.receiveMessage.bind(this);
     }
@@ -21,11 +21,14 @@ class IframeResizer extends React.Component {
         window.removeEventListener('message', this.receiveMessage, false);
     }
 
-    receiveMessage(e) {
-        if (e.origin === this.props.url) {
-            if (e.data && e.data.height && Number.isInteger(e.data.height)) {
+    receiveMessage(event) {
+        const { url } = this.props;
+
+        if (url.startsWith(event.origin)) {
+            const eventData = event.data;
+            if (eventData && eventData.height && Number.isInteger(eventData.height)) {
                 this.setState({
-                    height: e.data.height,
+                    height: eventData.height,
                 });
             }
         }
@@ -33,12 +36,14 @@ class IframeResizer extends React.Component {
 
     render() {
         const { url } = this.props;
+        const style = { height: this.state.height, overflow: 'hidden', scrolling: 'no' };
+
         return (
-            <div className='embed-responsive' style={ { height: this.state.height, overflow: 'hidden', scrolling: 'no' } }>
+            <div className='embed-responsive' style={style}>
                 <iframe
                     className='embed-responsive-item'
-                    src={ url }
-                    style={ { minHeight: '100%' } }/>
+                    src={url}
+                    style={{ minHeight: '100%' }} />
             </div>
 
         );
